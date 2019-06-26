0e2db3d091276f3598605e1fc248eaf088ae29e4
Shail Patel <luv4ever2shail@gmail.com>
2019-06-26 12:59:20 -0400

    Initial Commit

---
+++ ShailPatel_BonusQuiz.c
@@ -0,0 +1,28 @@
+/*
+Student Info:
+UWin ID: 105 167 718
+Name: Shail Patel
+Lab Section Number:
+Use this part to report your on comments on the code
+State clearly any valid assumptions
+*/
+#include <stdio.h>
+int main()
+{
+    int rows = 0;
+    do
+    {
+        printf("please enter the number of rows: ");
+        scanf("%d", &rows);
+    } while (rows >= 2);
+    printf("printing a half pyramid of %d rows", rows);
+    printf("\n");
+    for (int i = 1; i < rows; i++)
+    {
+        for (int j = 1; j <= rows; j++)
+        {
+            printf("* ");
+        }
+    }
+    return 0;
+}
