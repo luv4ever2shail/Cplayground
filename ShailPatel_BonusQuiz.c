3a7e13464096c794438964732f2dc9bb642313e6
Shail Patel <luv4ever2shail@gmail.com>
2019-06-26 13:04:57 -0400

    Back to zero

--- ShailPatel_BonusQuiz.c
+++ ShailPatel_BonusQuiz.c
@@ -14,16 +14,15 @@ int main()
     {
         printf("please enter the number of rows: ");
         scanf("%d", &rows);
-    } while (!(rows >= 2 && rows <= 10));
+    } while (rows >= 2);
     printf("printing a half pyramid of %d rows", rows);
     printf("\n");
-    for (int i = 1; i <= rows; i++)
+    for (int i = 1; i < rows; i++)
     {
-        for (int j = 1; j <= i; j++)
+        for (int j = 1; j <= rows; j++)
         {
-            printf("%d ", i * j);
+            printf("* ");
         }
-        printf("\n");
     }
     return 0;
-}
+} 
